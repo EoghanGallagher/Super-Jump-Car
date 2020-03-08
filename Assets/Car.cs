f901a5bf219a23947152a6ef08e66a6f450c986c
Eoghan Gallagher <eoghan.gallagher@gmail.com>
2020-03-08 20:31:33 +0000

    Added Test Movement Script

---
+++ Assets/Car.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+//[RequireComponent(typeof(Rigidbody))]
+public class Car : MonoBehaviour
+{
+    Rigidbody m_Rigidbody;
+    [SerializeField] private float m_Speed;
+
+    void Start()
+    {
+        //Fetch the Rigidbody component you attach from your GameObject
+        m_Rigidbody = GetComponent<Rigidbody>();
+        //Set the speed of the GameObject
+        m_Speed = 50.0f;
+    }
+
+    private void Update()
+    {
+       
+        transform.Translate(Vector3.right * Time.deltaTime * m_Speed );
+
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            //Move the Rigidbody forwards constantly at speed you define (the blue arrow axis in Scene view)
+            m_Rigidbody.velocity = transform.forward * m_Speed;
+        }
+
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            //Move the Rigidbody backwards constantly at the speed you define (the blue arrow axis in Scene view)
+            m_Rigidbody.velocity = -transform.forward * m_Speed;
+        }
+
+        // if (Input.GetKey(KeyCode.RightArrow))
+        // {
+        //     //Rotate the sprite about the Y axis in the positive direction
+        //     transform.Rotate(new Vector3(0, 1, 0) * Time.deltaTime * m_Speed, Space.World);
+        // }
+
+        // if (Input.GetKey(KeyCode.LeftArrow))
+        // {
+        //     //Rotate the sprite about the Y axis in the negative direction
+        //     transform.Rotate(new Vector3(0, -1, 0) * Time.deltaTime * m_Speed, Space.World);
+        // }
+    }
+}
